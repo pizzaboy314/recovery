59349ff87c3eef442d38e4ae33cac56f17bad1fb
Bryan Spahr <bspahr314@gmail.com>
2014-03-21 14:23:08 -0500

    DEMO1

---
+++ Unity/Assets/changeScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnGUI(){
+		if(GUI.Button(new Rect(400, 650, 200, 40), "Start"))
+			Application.LoadLevel("level1");
+	}
+	                  
+}
