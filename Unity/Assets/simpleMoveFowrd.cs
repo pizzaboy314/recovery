07b3bf9d94ff4e415b3ec178f634bb7ba9f1980d
sgm1 <sgmathai@att.net>
2014-03-27 11:39:37 -0500

    Sample intro, started

---
+++ Unity/Assets/simpleMoveFowrd.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simpleMoveFowrd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = this.transform.up * 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator zeroLater(){
+		yield return new WaitForSeconds(5f);
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.angularVelocity = Vector3.zero;
+	}
+
+
+	public void OnCollisionEnter(Collision other){
+		rigidbody.useGravity = true;
+		rigidbody.angularVelocity = transform.up ;
+		StartCoroutine(zeroLater());
+	}
+}
