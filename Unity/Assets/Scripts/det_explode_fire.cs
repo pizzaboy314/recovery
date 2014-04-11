5d316127cfd87dee71469b505216bbce187c9fff
sgm1 <sgmathai@att.net>
2014-04-11 16:47:01 -0500

    started path

--- Unity/Assets/Scripts/det_explode_fire.cs
+++ Unity/Assets/Scripts/det_explode_fire.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class det_explode_fire : MonoBehaviour {
 
 	private static int numNades = 0;
+	private static float fogDensity = -1f;
 	public float lifeSpan;
 
 	// Use this for initialization
 	void Start () {
 		++numNades;
 		RenderSettings.fogColor = new Color(0.06f, 0.02f, 0f);
+		if (fogDensity == -1)
+			fogDensity  = RenderSettings.fogDensity;
 		RenderSettings.fogDensity *= 0.5f;
+
 		audio.Play();
 	}
 	
@@ -21,7 +25,7 @@ public class det_explode_fire : MonoBehaviour {
 		if(lifeSpan <= 0){
 			--numNades;
 			if (numNades == 0){
-				RenderSettings.fogDensity = 0.1f;
+				RenderSettings.fogDensity = fogDensity;
 				RenderSettings.fogColor = Color.black;
 			}
 			Destroy(gameObject);
--- Unity/Assets/Scripts/follow.cs
+++ Unity/Assets/Scripts/follow.cs
@@ -1,60 +1,107 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class follow : MonoBehaviour {
-	public GameObject toFollow;
-	private Animator ani;
-	private bool waitingUpdate;
-	private float lastXAng;
+namespace RobotAI
+{
+		public class follow : MonoBehaviour
+		{
+				public GameObject toFollow;
+				public Vector3 toFollowPath;
+				public float maxPathSpeed;
+				public float maxFollowSpeed;
+				private Animator ani;
+				private bool foundPlayer;
+				private float lastXAng;
 
-	// Use this for initialization
-	void Start () {
-		ani = GetComponent<Animator>();
-		waitingUpdate = false;
-		lastXAng = 0;
-	}
+				// Use this for initialization
+				void Start ()
+				{
+						ani = GetComponent<Animator> ();
+						foundPlayer = true;
+						lastXAng = 0;
+				}
 
-	//private IEnumerator moveUpdate(){
-	private void moveUpdate(){
-		waitingUpdate = true;
-		Vector3 angle = transform.InverseTransformPoint(toFollow.transform.position);
-		float s = Vector3.Distance(transform.position, toFollow.transform.position) / 10f;
-		Debug.Log("L:" + angle.x);
-		//if (Mathf.Abs(lastXAng - angle.x) > 1)
-		if (s  < 0.3)//if close, turn faster
-			ani.SetFloat("Turn", Mathf.Lerp (lastXAng, angle.x / Mathf.PI, 0f));
-		else
-			ani.SetFloat("Turn", Mathf.Lerp (lastXAng, angle.x / Mathf.PI, 10f));
-		//else
-		//	ani.SetFloat("Turn", s);
-		lastXAng = angle.x;
-		if (s > 0.5){
-			ani.SetBool("Punching", false);
-			if (angle.x > Mathf.PI / 2)
-				ani.SetFloat("Forward", 0.3f);
-			else
-				ani.SetFloat("Forward", 1f);
-		}
-		else if (s > 0.15){
-			ani.SetBool("Punching", false);
-			if (angle.x > Mathf.PI / 2)
-				ani.SetFloat("Forward", 0.3f);
-			else
-				ani.SetFloat("Forward", s * 2f);
-		}
-		else{
-			ani.SetFloat("Forward", 0.00f);
-			ani.SetBool("Punching", true);
-		}
-		//yield return null;
-		waitingUpdate = false;
-	}
+				private void movePathUpdate ()
+				{
+						//waitingUpdate = true;
+						Vector3 angle = transform.InverseTransformPoint (toFollowPath);
+						float s = Vector3.Distance (transform.position, toFollowPath) / 10f;
+						//Debug.Log ("L:" + angle.x);
+						//if (Mathf.Abs(lastXAng - angle.x) > 1)
+						if (s < 0.3)//if close, turn faster
+								ani.SetFloat ("Turn", Mathf.Lerp (lastXAng, angle.x / Mathf.PI, 0f));
+						else
+								ani.SetFloat ("Turn", Mathf.Lerp (lastXAng, angle.x / Mathf.PI, 10f));
+						//else
+						//	ani.SetFloat("Turn", s);
+						lastXAng = angle.x;
+						if (s > maxPathSpeed) {
+								ani.SetBool ("Punching", false);
+								if (angle.x > Mathf.PI / 2)
+										ani.SetFloat ("Forward", 0.2f);
+								else
+										ani.SetFloat ("Forward", maxPathSpeed);
+						} else if (s > (maxPathSpeed / 2f)) {
+								ani.SetBool ("Punching", false);
+								if (angle.x > Mathf.PI / 2)
+										ani.SetFloat ("Forward", (maxPathSpeed / 2f < 0.2f)?(maxPathSpeed / 2f): 0.2f);
+								else
+										ani.SetFloat ("Forward", s * 2f);
+						} else {
+								ani.SetFloat ("Forward", 0.00f);
+								ani.SetBool ("Punching", true);
+						}
+						//yield return null;
+						//waitingUpdate = false;
+				}
+
+				//private IEnumerator moveUpdate(){
+				
+				private void moveUpdate ()
+				{
+						Debug.Log("IN HERE");
+						float factor1 = 0.15f / maxFollowSpeed;
+						float factor2 = 0.3f / maxFollowSpeed;
+						//waitingUpdate = true;
+						Vector3 angle = transform.InverseTransformPoint (toFollow.transform.position);
+						float s = Vector3.Distance (transform.position, toFollow.transform.position) / 10f;
+						Debug.Log ("L:" + angle.x);
+						//if (Mathf.Abs(lastXAng - angle.x) > 1)
+						if (s < 0.3)//if close, turn faster
+								ani.SetFloat ("Turn", Mathf.Lerp (lastXAng, angle.x / Mathf.PI, 0f));
+						else
+								ani.SetFloat ("Turn", Mathf.Lerp (lastXAng, angle.x / Mathf.PI, 10f));
+						//else
+						//	ani.SetFloat("Turn", s);
+						lastXAng = angle.x;
+						if (s > maxFollowSpeed) {
+								ani.SetBool ("Punching", false);
+								if (angle.x > Mathf.PI / 2)
+										ani.SetFloat ("Forward", (maxPathSpeed / 2f < 0.2f)?(maxPathSpeed / 2f): 0.2f);
+								else
+										ani.SetFloat ("Forward", maxFollowSpeed);
+						} else if (s > (maxFollowSpeed / 2f)) {
+								ani.SetBool ("Punching", false);
+								if (angle.x > Mathf.PI / 2)
+										ani.SetFloat ("Forward", 0.2f);
+								else
+										ani.SetFloat ("Forward", s * 2f);
+						} else {
+								ani.SetFloat ("Forward", 0.00f);
+								ani.SetBool ("Punching", true);
+						}
+						//yield return null;
+						//waitingUpdate = false;
+				}
+				
 	
-	// Update is called once per frame
-	void Update () {
-		if (!waitingUpdate){
-			moveUpdate();
-			//StartCoroutine (moveUpdate());
+				// Update is called once per frame
+				void Update ()
+				{
+							if (foundPlayer)
+								moveUpdate ();
+							else
+								movePathUpdate();
+				}
 		}
-	}
 }
