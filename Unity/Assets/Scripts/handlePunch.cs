5356dda3274da9b9b2329a4779308987a896bddd
sgm1 <sgmathai@att.net>
2014-04-18 17:50:13 -0500

    working on interaction

---
+++ Unity/Assets/Scripts/handlePunch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class handlePunch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GetHit(){
+		Debug.Log("STUFF GOT HERE");
+
+	}
+}
---
+++ Unity/Assets/Scripts/lookAt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookAt : MonoBehaviour {
+	public GameObject toFocus;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt (toFocus.transform.position);
+	}
+}
