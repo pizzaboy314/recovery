d2a39a8bf9e200a97393b2d8abe2b26f52bb7c4f
sgm1 <sgmathai@att.net>
2014-04-26 11:11:00 -0500

    moved around scripts, updated death cam

---
+++ Unity/Assets/SFX/dropGrate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dropGrate : MonoBehaviour {
+	public GameObject grateToDrop;
+	private bool applied;
+
+	// Use this for initialization
+	void Start () {
+		applied = false;
+		if (grateToDrop.rigidbody == null)
+			grateToDrop.AddComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!applied)
+			grateToDrop.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.name == grateToDrop.name)
+			return;
+		if (applied)
+			return;
+		applied = true;
+		grateToDrop.rigidbody.constraints = RigidbodyConstraints.None;
+		grateToDrop.rigidbody.useGravity = true;
+		Debug.Log("Appliing forces");
+		grateToDrop.rigidbody.AddForce ((Vector3.down + Vector3.left) * 100);
+		grateToDrop.rigidbody.AddRelativeTorque((Vector3.down + Vector3.left) * 1000);
+	}
+}
