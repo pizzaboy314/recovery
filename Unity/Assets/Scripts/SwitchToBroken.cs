45fb6cb765a1211025d7c10cd03068ce8dc82936
Bryan Spahr <bspahr314@gmail.com>
2014-04-26 12:45:49 -0500

    crashing ship becomes broken when it hits now

---
+++ Unity/Assets/Scripts/SwitchToBroken.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchToBroken : MonoBehaviour {
+	public GameObject failingShip;
+	public GameObject brokenShip;
+	private bool isBroken;
+
+	// Use this for initialization
+	void Start () {
+		isBroken = false;
+		failingShip.renderer.enabled = true;
+		brokenShip.renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnCollisionEnter(Collision col){
+		if (isBroken == false) {
+			isBroken = true;
+			failingShip.renderer.enabled = false;
+			brokenShip.renderer.enabled = true;
+		}
+	}
+}
