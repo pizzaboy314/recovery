1515de6cbd638a10648ad6cfe5405709f1231387
sgm1 <sgmathai@att.net>
2014-04-26 12:52:14 -0500

    unpdated start screen a tad

--- Unity/Assets/Scripts/changeScene.cs
+++ Unity/Assets/Scripts/changeScene.cs
@@ -2,20 +2,25 @@
 using System.Collections;
 
 public class changeScene : MonoBehaviour {
-
+	private int lastW, lastH;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (lastW != Screen.width || lastH != Screen.height){
+			guiTexture.pixelInset = new Rect(0, 0, Screen.width, Screen.height);
+			lastH = Screen.height;
+			lastW = Screen.width;
+		}
 	}
 
 	private void OnGUI(){
-		if(GUI.Button(new Rect(400, 650, 200, 40), "Start"))
-			Application.LoadLevel("level1");
+		int x = Screen.width / 2 - 400/2;
+		int y = Screen.height * 3 / 4 - 200/2;
+		if(GUI.Button(new Rect(x, y, 200, 40), "Start"))
+			Application.LoadLevel("intro");
 	}
 	                  
 }
