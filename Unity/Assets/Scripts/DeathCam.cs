fa9a9b5f139d8cadcf0511e509040a660c56e08e
Bryan Spahr <bspahr314@gmail.com>
2014-04-25 20:56:59 -0500

    scene change trigger script and half working death cam

---
+++ Unity/Assets/Scripts/DeathCam.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathCam : MonoBehaviour {
+
+	public float lifeSpan = 4f;
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+		cam.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeSpan -= Time.deltaTime;
+		if(lifeSpan <= 0){
+			cam.enabled = false;
+			Destroy(gameObject);
+		}
+	}
+}
