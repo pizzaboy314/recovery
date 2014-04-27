3595f8fe8b70b9b76c243a27e3ae842c39ec07d0
Bryan Spahr <bspahr314@gmail.com>
2014-04-27 01:51:23 -0500

    character dialog for intro level

---
+++ Unity/Assets/Scripts/CharDialogHandlerIntro.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharDialogHandlerIntro : MonoBehaviour {
+
+	public AudioClip endOfThatShip;
+
+	// Use this for initialization
+	void Start () {
+		audio.clip = endOfThatShip;
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void playClip(AudioClip clip){
+		audio.clip = clip;
+		audio.Play ();
+	}
+}
---
+++ Unity/Assets/Scripts/LocBasedDialogClip.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocBasedDialogClip : MonoBehaviour {
+
+	public AudioClip clipToPlay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.name == "Head Camera") {
+			col.gameObject.SendMessage("playClip", clipToPlay);
+		}
+	}
+}
