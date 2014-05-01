968e49b2f3dfb38ae89b76bad120cffa86c418fb
sgm1 <sgmathai@att.net>
2014-04-30 20:39:56 -0500

    elevator ready to be droped, negative valeu for distance should for downward

---
+++ Unity/Assets/MoveUpBy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MoveUpBy : MonoBehaviour {
+	public bool isMoving = false;
+	public float distance = 1.0f;
+	public float timeToDest = 1.0f;
+	private Vector3 startPos;
+	private float tElapsed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isMoving && timeToDest >= tElapsed){
+			tElapsed += Time.deltaTime;
+			transform.position = startPos + Vector3.up * Mathf.Lerp (0.0f, distance, (tElapsed / timeToDest));
+			/*
+			Transform[] allChildren = GetComponentsInChildren<Transform>();
+			for(int i = 0; i < allChildren.Length; ++i){
+				Transform t = allChildren[i];
+				//t.localPosition += Vector3.up * distance * Time.deltaTime / timeToDest;
+				t.position += Vector3.up * distance * Time.deltaTime / timeToDest;
+			}
+			*/
+		}
+	}
+}
