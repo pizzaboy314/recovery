b0163690313091f10336f84223a0458d2c14e613
Bryan Spahr <bspahr314@gmail.com>
2014-05-01 17:53:31 -0500

    safety net and black skybox

---
+++ Unity/Assets/Scripts/safetyNet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class safetyNet : MonoBehaviour {
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col){
+		player.SendMessage("reset");
+	}
+}
