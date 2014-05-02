509a3f56dc3fc08f48f34eafe179ebdc96db2ea5
Bryan Spahr <bspahr314@gmail.com>
2014-05-02 00:04:24 -0500

    cheat to open shortcut

---
+++ Unity/Assets/Scripts/MoveGrates.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGrates : MonoBehaviour {
+
+	public bool isMoving = false;
+	public float distance = 1.0f;
+	public float timeToDest = 1.0f;
+	private Vector3 startPos;
+	private float tElapsed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		isMoving = Input.GetButton("Grates");
+		if (isMoving && timeToDest >= tElapsed) {
+			tElapsed += Time.deltaTime;
+			transform.position = startPos + Vector3.up * Mathf.Lerp (0.0f, distance, (tElapsed / timeToDest));
+		} else {
+			isMoving = false;
+		}
+	}
+}
